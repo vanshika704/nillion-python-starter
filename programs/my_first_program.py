f1d087f2043720dbbb9769a6b9da2df6f6950dc5
vanshika704 <sharmavanshi704@gmail.com>
2024-07-02 16:58:26 +0000

    my new nada program

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,12 +1,23 @@
 from nada_dsl import *
 
-
 def nada_main():
     party1 = Party(name="Party1")
     my_int1 = SecretInteger(Input(name="my_int1", party=party1))
     my_int2 = SecretInteger(Input(name="my_int2", party=party1))
 
-    # write the computation for your program here - use my_int1 and my_int2 as inputs
-    # make sure you change the output below to be your new output
+    # Perform addition, subtraction, multiplication, and division
+    addition_result = my_int1 + my_int2
+    subtraction_result = my_int1 - my_int2
+    multiplication_result = my_int1 * my_int2
+    division_result = my_int1 / my_int2  # Note: Division assumes my_int2 is non-zero
 
-    return [Output(my_int1, "my_output", party1)]
+    # Define outputs
+    outputs = [
+        Output(addition_result, "addition_output", party1),
+        Output(subtraction_result, "subtraction_output", party1),
+        Output(multiplication_result, "multiplication_output", party1),
+        Output(division_result, "division_output", party1)
+    ]
+
+    return outputs
+
